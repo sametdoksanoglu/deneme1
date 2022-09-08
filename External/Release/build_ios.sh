757537f1a8a14726574aef3668ea162aeb2d0916
Samet Doksanoglu <99030991+sametdoksanoglu@users.noreply.github.com>
2022-09-08 15:03:07 +0300

    Update build_ios.sh

--- External/Release/build_ios.sh
+++ External/Release/build_ios.sh
@@ -1,3 +1,9 @@
 echo "Build ios"
 echo "Test Commit: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
 echo "Profiling_Support: ${Profiling_Support}"
+if [ $PROFILING_SUPPORT == 'false' ]
+  then
+    echo "profile false"
+  else
+     echo "profile true"
+fi
