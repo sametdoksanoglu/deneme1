cc4e1dc977ceb2bd7436cd7b9a495e95cb02f4f4
Samet Doksanoglu <99030991+sametdoksanoglu@users.noreply.github.com>
2022-09-08 15:30:43 +0300

    Update build_android.sh

--- External/Release/build_android.sh
+++ External/Release/build_android.sh
@@ -1,7 +1,7 @@
 echo "Build Android"
 echo "Test Commit: ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
 echo "Profiling_Support: ${Profiling_Support}"
-if [ $PROFILING_SUPPORT == 'false' ]
+if [[ $PROFILING_SUPPORT == 'false' ]]
   then
     echo "profile false"
   else
