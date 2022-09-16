1dc7073ff22eb3226b297ae875c4ed87d755845c
Samet Doksanoglu <99030991+sametdoksanoglu@users.noreply.github.com>
2022-09-16 17:57:19 +0300

    Add nightmare_bundle.sh

---
+++ External/Release/nightmare_bundle.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# Jenkins Config
+WORKSPACE=${WORKSPACE}
+ENVIRONMENT=${ENVIRONMENT}
+BUNDLE_NAME=${NIGHTMARE_BUNDLE_NAME}
+BUNDLE_QUALITY=${BUNDLE_QUALITY}
+PLATFORM=${PLATFORM}
+
+printf "\n\n"
+printf "%-50s : %10s\n" "Key"  "Value"
+printf "=%.0s"  $(seq 1 100)
+printf "\n"
+printf "%-50s : %10s\n" "WORKSPACE" ${WORKSPACE}
+printf "%-50s : %10s\n" "ENVIRONMENT" ${ENVIRONMENT}
+printf "%-50s : %10s\n" "BUNDLE_NAME" ${NIGHTMARE_BUNDLE_NAME}
+printf "%-50s : %10s\n" "BUNDLE_QUALITY" ${BUNDLE_QUALITY}
+printf "%-50s : %10s\n" "PLATFORM" ${PLATFORM}
+printf "\n\n"
