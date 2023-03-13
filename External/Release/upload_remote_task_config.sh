2f617073b3bda4b30c4b7c731b7cd01d4e159c24
Samet Doksanoglu <99030991+sametdoksanoglu@users.noreply.github.com>
2023-03-13 17:19:33 +0300

    fix

--- External/Release/upload_remote_task_config.sh
+++ External/Release/upload_remote_task_config.sh
@@ -3,7 +3,7 @@
 S3_DEV_REMOTE_TASK_CONFIG_PATH="delete-remote-task-config"
 S3_PROD_REMOTE_TASK_CONFIG_PATH="delete-remote-task-config"
 
-if [ aws s3 ls "s3://$S3_DEV_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json" | grep -iow "$BUILD_NUMBER.json" ]
+if [ ! -z $(aws s3 ls "s3://$S3_DEV_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json" | grep -iow $BUILD_NUMBER.json) ]
 then 
   echo "Remote Task Config Json Already Exists. Nothing Uploaded";
   exit 0
@@ -12,8 +12,8 @@ fi
 touch $BUILD_NUMBER.json
 if [ $ENVIRONMENT == 'prod' ]
 then
-  aws s3 cp  "$BUILD_NUMBER.json" "s3://$S3_DEV_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json"
-  if [ aws s3 ls "s3://$S3_DEV_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json" | grep -iow "$BUILD_NUMBER.json" ]
+  aws s3 cp  $BUILD_NUMBER.json "s3://$S3_DEV_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json"
+  if [ ! -z $(aws s3 ls "s3://$S3_DEV_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json" | grep -iow $BUILD_NUMBER.json) ]
   then 
     echo "Remote Task Config Json Succesfully Uploaded"; 
   else 
@@ -21,8 +21,8 @@ then
     exit 1
   fi
 else
-  aws s3 cp  "$BUILD_NUMBER.json" "s3://$S3_PROD_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json"
-  if [ aws s3 ls "s3://$S3_PROD_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json" | grep -iow "$BUILD_NUMBER.json" ]
+  aws s3 cp  $BUILD_NUMBER.json "s3://$S3_PROD_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json"
+  if [ ! -z $(aws s3 ls "s3://$S3_PROD_REMOTE_TASK_CONFIG_PATH/$BUILD_NUMBER.json" | grep -iow $BUILD_NUMBER.json) ]
   then 
     echo "Remote Task Config Json Succesfully Uploaded"; 
   else 
