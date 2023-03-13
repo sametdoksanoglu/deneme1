100bc951fd6db8312237218df9b58ef517738429
Samet Doksanoglu <99030991+sametdoksanoglu@users.noreply.github.com>
2023-03-13 16:57:31 +0300

    Fix file name

--- External/Release/upload_remote_level.sh
+++ External/Release/upload_remote_level.sh
@@ -3,7 +3,7 @@
 S3_DEV_REMOTE_LEVEL_PATH="delete-remote-level/versions/"
 S3_PROD_REMOTE_LEVEL_PATH="delete-remote-level/versions/"
 
-if [ aws s3 ls "s3://$S3_PROD_REMOTE_LEVEL_PATH/$BUILD_NUMBER.json" | grep -iow $BUILD_NUMBER.json ]
+if [ aws s3 ls "s3://$S3_PROD_REMOTE_LEVEL_PATH/$BUILD_NUMBER.json" | grep -iow "$BUILD_NUMBER.json" ]
 then 
   echo "Remote Level Json Already Exists. Nothing Uploaded"; 
   exit 0
@@ -13,7 +13,7 @@ touch $BUILD_NUMBER.json
 if [ $ENVIRONMENT == 'prod' ]
 then
   aws s3 cp  $BUILD_NUMBER.json "s3://$S3_PROD_REMOTE_LEVEL_PATH/$BUILD_NUMBER.json"
-  if [ aws s3 ls "s3://$S3_PROD_REMOTE_LEVEL_PATH/$BUILD_NUMBER.json" | grep -iow $BUILD_NUMBER.json ]
+  if [ aws s3 ls "s3://$S3_PROD_REMOTE_LEVEL_PATH/$BUILD_NUMBER.json" | grep -iow "$BUILD_NUMBER.json" ]
   then 
     echo "Remote Level Json Succesfully Uploaded"; 
   else 
@@ -21,8 +21,8 @@ then
     exit 1
   fi
 else
-  aws s3 cp  $BUILD_NUMBER.json "s3://$S3_DEV_REMOTE_LEVEL_PATH/$BUILD_NUMBER.json"
-  if [ aws s3 ls "s3://$S3_DEV_REMOTE_LEVEL_PATH/$BUILD_NUMBER.json" | grep -iow $BUILD_NUMBER.json ]
+  aws s3 cp  "$BUILD_NUMBER.json" "s3://$S3_DEV_REMOTE_LEVEL_PATH/$BUILD_NUMBER.json"
+  if [ aws s3 ls "s3://$S3_DEV_REMOTE_LEVEL_PATH/$BUILD_NUMBER.json" | grep -iow "$BUILD_NUMBER.json" ]
   then 
     echo "Remote Level Json Succesfully Uploaded"; 
   else 
