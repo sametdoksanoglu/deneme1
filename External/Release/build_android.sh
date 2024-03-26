ed7e2a7e5799807613133bc4d1c563b6dc161e5c
sametdoksanoglu <samet@dreamgames.com>
2024-03-26 18:10:50 +0300

    Change

--- External/Release/build_android.sh
+++ External/Release/build_android.sh
@@ -9,7 +9,7 @@ already_in_prod_bundles=(1,2,3)
 if [[ ${#already_in_prod_bundles[@]} -eq 0 ]]; then
   echo "\033[32mAll bundles are copied to prod successfully\033[0m\n"
 else
-  echo "\033[31mSome bundles already in prod. They are not replaced. Please contact with Techops Team\033[0m"
-  echo "\033[31mThe bundles are: \n\033[0m"
+  echo "\033[31mSome bundles already in prod. They are not updated. If they should be, please contact with Techops Team\033[0m"
+  echo "\033[31mThe bundles are: \033[0m"
   echo "  \033[31m$bundle_parent_folder/$PLATFORM/$bundle_group_label/$bundle_group_name/$bundle_name\033[0m"
 fi
